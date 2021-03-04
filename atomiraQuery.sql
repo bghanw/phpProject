75129c97b326eff32ce9e77d247ba510fa63620b
bghanw <bghanw@bbcag.ch>
2021-03-04 16:51:59 +0100

    Qureies and ERM
    Fiext tel type to varchare and Queries format for Company and paticipant

--- atomiraQuery.sql
+++ atomiraQuery.sql
@@ -3,8 +3,10 @@ SET autocommit = ON;
 
 
 -- Compnay Erstellen
-INSERT INTO `company`(`company`, `tel`, `address`, `postal`, `city`) VALUES ('BBC Ag','0041 77 993 00 54','Bernstrasse 14','3004','Bern');
-
+INSERT INTO `company`(`company`, `tel`, `address`, `postal`, `city`) 
+	VALUES ('BBC Ag','0041 77 993 00 54','Bernstrasse 14','3004','Bern'),
+			('Gibb','0041 88 678 00 89','Lorraniestrasse 41','3001','Bern');
+            
 -- Compnay Löschen
 DELETE FROM `company` WHERE `company`.`id`= 2;
 
@@ -12,7 +14,9 @@ select * from company;
 
 
 -- Participant Erstellen
-INSERT INTO `participant`(`firstname`, `lastname`, `tel`, `email`, `password`) VALUES ('Wais','Ghanizada','0041 77 993 00 55','wais@bbcag.ch','123456');
+INSERT INTO `participant`(`firstname`, `lastname`, `tel`, `email`, `password`)
+	VALUES ('Wais','Ghanizada','0041 77 993 00 55','wais@bbcag.ch','123456'),
+			('Reini','Graf','0041 77 883 00 99','reini@bbcag.ch','123456');
 
 -- Compnay Löschen
 DELETE FROM `participant` WHERE `paricipant`.`id`= 2;
