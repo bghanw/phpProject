9306b814405ea9720265315db1dcec88f772f845
bghanw <bghanw@bbcag.ch>
2021-03-04 16:41:17 +0100

    Participant query
    participant query added and also paticipant id set to auto increment

--- atomiraQuery.sql
+++ atomiraQuery.sql
@@ -10,3 +10,12 @@ DELETE FROM `company` WHERE `company`.`id`= 2;
 
 select * from company;
 
+
+-- Participant Erstellen
+INSERT INTO `participant`(`firstname`, `lastname`, `tel`, `email`, `password`) VALUES ('Wais','Ghanizada','0041 77 993 00 55','wais@bbcag.ch','123456');
+
+-- Compnay Löschen
+DELETE FROM `participant` WHERE `paricipant`.`id`= 2;
+
+select * from participant;
+
